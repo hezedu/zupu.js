431a394d1c33eb2aaee4f04ed3907ffa0b34e287
hezedu@hotmail.com <hezedu@hotmail.com>
2016-08-08 18:46:30 +0800

    2

---
+++ zupu2.js
@@ -0,0 +1,19 @@
+
+//style Init
+document.write("<style>\
+.dw_zupu_show{padding:10px;font-family:'Courier new','微软雅黑';word-wrap:break-word;font-size:16px;}\
+.dw_zupu_first:hover{background:#FFEBCD;}\
+.dw_zupu_wai{background:fuchsia;font-size:12px}\
+.dw_zupu_Function , .dw_same{color:blue}\
+.dw_zupu_same1{color:darkgoldenrod}.dw_zupu_Object{color:red}\
+.dw_zupu_Array{color:orangered}.dw_zupu_String{color:yellowgreen}.dw_zupu_Number{color:green}\
+.dw_zupu_Boolean{color:darkgoldenrod}\
+.dw_zupu_Undefined , .dw_zupu_Null{color:silver}.dw_zupu_zu1{color:#fff;}\
+.dw_zupu_first:hover .dw_zupu_zu1{color:#999}\
+.dw_zupu_count{float:right;color:dimgray;font-size:12px;font-family:'Georgia'}\
+.dw_zupu_fund{display:none;font-size:14px;color:#001432;}\
+.dw_zupu_funb{cursor:pointer}\
+.dw_zupu_funj{display:inline-block;height:14px;width:14px;border:1px solid #666;line-height:14px;text-align:center;margin:1px}\
+</style>");
+
+
